bca7f4eec126d1e57bcbdb62cd4ad9475d34068d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-21 09:55:19 -0300

    Creando Routing con React Router DOM 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/Login";
+import Registrar from "./pages/Registrar";
+import RecuperarPassword from "./pages/RecuperarPassword";
+import NuevoPassword from "./pages/NuevoPassword";
+import ConfirmarCuenta from "./pages/ConfirmarCuenta";
 
 function App() {
   return (
-    <div>
-      <h1>UpTask</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route index path="registrar" element={<Registrar />} />
+          <Route
+            index
+            path="recuperar-password"
+            element={<RecuperarPassword />}
+          />
+          <Route
+            index
+            path="recuperar-password/:token"
+            element={<NuevoPassword />}
+          />
+          <Route index path="confirmar/:id" element={<ConfirmarCuenta />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/RecuperarPassword.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecuperarPassword = () => {
+  return <div>RecuperarPassword</div>;
+};
+
+export default RecuperarPassword;
