84d882841b2149edf2b03386a37858d3e95286de
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-25 13:04:59 -0300

    Revisando el Token para Autenticar al usuario

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
@@ -7,10 +8,28 @@ const AuthProvider = ({ children }) => {
   const [usuario, setUsuario] = useState({});
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      console.log("Hay token");
-    }
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return;
+      }
+
+      // Configuración para enviar el token por headers
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      try {
+        const { data } = await clienteAxios.get("/usuarios/perfil", config);
+        setAuth(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    autenticarUsuario();
   }, []);
 
   return (
