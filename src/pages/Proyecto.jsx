4d99e381826188ebe12894e6c4290b9b8fd89a97
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-27 16:58:24 -0300

    Mostrando las Tareas de un Proyecto

--- src/pages/Proyecto.jsx
+++ src/pages/Proyecto.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
+import Tarea from "../components/Tarea";
 
 const Proyecto = () => {
   const [modal, setModal] = useState(false);
@@ -62,6 +63,24 @@ const Proyecto = () => {
         </svg>
         Nueva Tarea
       </button>
+
+      <p className="text-gray-400 text-sm mt-5">
+        Tareas del Proyecto{" "}
+        <span className="font-bold text-gray-600">
+          {proyecto.tareas?.length}
+        </span>
+      </p>
+      <div className="bg-white shadow mt-10 rounded-lg">
+        {proyecto.tareas?.length ? (
+          proyecto.tareas?.map((tarea) => (
+            <Tarea key={tarea._id} tarea={tarea} />
+          ))
+        ) : (
+          <p className="text-center my-5 text-gray-400 p-10">
+            No hay tareas en este proyecto
+          </p>
+        )}
+      </div>
 
       <ModalFormularioTarea modal={modal} setModal={setModal} />
     </>
