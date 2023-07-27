19e81f8c4871106f22cd51b07437dcaec7cd41a2
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-27 15:10:15 -0300

    Creando el Botón para Añadir Tareas

--- src/pages/Proyecto.jsx
+++ src/pages/Proyecto.jsx
@@ -16,9 +16,30 @@ const Proyecto = () => {
   if (cargando) return "Cargando...";
 
   return (
-    <div className="flex justify-between ">
-      <h1 className="font-black text-4xl">{nombre}</h1>
-      <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+    <>
+      <div className="flex justify-between ">
+        <h1 className="font-black text-4xl">{nombre}</h1>
+        <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            />
+          </svg>
+          <Link to={`/proyectos/editar/${id}`} className="uppercase font-bold">
+            Editar
+          </Link>
+        </div>
+      </div>
+      <button className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center hover:bg-sky-600 transition-all duration-300">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -30,14 +51,12 @@ const Proyecto = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            d="M12 4.5v15m7.5-7.5h-15"
           />
         </svg>
-        <Link to={`/proyectos/editar/${id}`} className="uppercase font-bold">
-          Editar
-        </Link>
-      </div>
-    </div>
+        Nueva Tarea
+      </button>
+    </>
   );
 };
 
